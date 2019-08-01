d4265879a9ccf2b5e48935ca188c541cea4d538b
rogueyoshi <me@rogueyoshi.com>
2019-08-01 03:27:01 -0400

    Separated includes

---
+++ index.js
@@ -0,0 +1,11 @@
+	var topAppBar = new mdc.topAppBar.MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+
+	var patients_name = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#patients_name'));
+	//var patients_name_icon = new mdc.textField.MDCTextFieldIcon(document.querySelector('.mdc-text-field#patients_name .mdc-text-field__icon'));
+	var dob = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#dob'));
+	var phone = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#phone'));
+	
+	var address = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#address'));
+	var city = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#city'));
+	var phone = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#phone'));
+	var zip_code = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field#zip_code'));
